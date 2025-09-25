c8dafd46885bf46d869c5a9e6379ba1cf87f2915
Ethan Lafrenais <ethan.lafrenais@gmail.com>
2025-09-25 16:02:08 -0400

    Hack to disable collision between the staff and the galleon

---
+++ dinomod_enhanced/src/core/objhits.c
@@ -0,0 +1,13 @@
+#include "modding.h"
+
+#include "game/objects/object.h"
+#include "game/objects/object_id.h"
+#include "sys/map.h"
+
+RECOMP_HOOK("func_80027934") void galleon_staff_collision_hack(Object* obj, Object* otherObj) {
+    // @recomp: Turn off collision between player weapons and the galleon
+    if (otherObj->id == OBJ_staff && D_800B4A50 == MAP_FRONT_END) {
+        // Note: we technically only need to set this once, maybe there's a better way to do this?
+        otherObj->objhitInfo->unk_0x5a = 0;
+    }
+}
