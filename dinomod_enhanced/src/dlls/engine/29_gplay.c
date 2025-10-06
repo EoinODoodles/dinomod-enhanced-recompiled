299eb3162a506029ff431fb0f2a36da299c0c974
EoinODoodles <e.odrisceoil@gmail.com>
2025-10-06 14:51:38 +0100

    Added new implementation of Scarab UI patch

--- dinomod_enhanced/src/dlls/engine/29_gplay.c
+++ dinomod_enhanced/src/dlls/engine/29_gplay.c
@@ -3,6 +3,7 @@
 
 #include "PR/ultratypes.h"
 #include "sys/map_enums.h"
+#include "sys/main.h"
 
 #include "recomp/dlls/engine/29_gplay_recomp.h"
 
@@ -12,4 +13,14 @@ extern u16 sMapObjGroupBitKeys[];
 RECOMP_HOOK_DLL(gplay_ctor) void gplay_patch_map_object_group_flags() {
     sMapObjGroupBitKeys[MAP_EARTHWALKER_TEMPLE] = 0x36a; //Shares Walled City's flag
     sMapObjGroupBitKeys[MAP_BOSS_KAMERIAN_DRAGON] = 0x5dc; //Shares the same flag as the rest of Dragon Rock (Bottom)
+}
+
+/** Checks if the Scarab collection cutscene has already played, and if so unlocks the Scarab UI
+  * (NOTE: the Scarab object DLL has also been patched to set the Scarab UI bit upon collection, so no need to check on every update!
+  *  This is mostly intended for players who start out playing unmodded, since it'll put the Scarab UI into its correct state) 
+  */
+RECOMP_HOOK_RETURN_DLL(gplay_start_game) void dll_gplay_hook_enable_scarabs_if_already_collected() {
+    if (get_gplay_bitstring(0x910) && get_gplay_bitstring(0x919) == 0){
+        set_gplay_bitstring(0x919, 1);
+    }
 }
---
+++ dinomod_enhanced/src/dlls/objects/297_scarab.c
@@ -0,0 +1,57 @@
+#include "modding.h"
+#include "recomputils.h"
+
+#include "dll.h"
+#include "sys/dll.h"
+#include "game/objects/object.h"
+#include "game/objects/object_id.h"
+#include "sys/main.h"
+#include "sys/objects.h"
+#include "sys/objtype.h"
+
+#include "dlls/engine/6_amsfx.h"
+#include "dlls/objects/210_player.h"
+#include "recomp/dlls/_asm/297_recomp.h"
+
+typedef struct {
+s32 soundHandle;
+s8 unk4[0x10 - 0x4];
+f32 unk10; //y
+s16 unk14;
+s8 unk16[0x18 - 0x16];
+s16 unk18;
+s16 unk1A;
+s8 unk1C[0x20 - 0x1C];
+s16 unk20;
+s16 unk22;
+s16 unk24;
+u8 unk26;
+s8 unk27[0x29 - 0x27];
+u8 unk29; //scarabTypeIndex
+} ScarabState;
+
+typedef struct {
+ObjCreateInfo base;
+s16 unk18;
+s16 unk1A;
+} ScarabCreateInfo;
+
+typedef union {
+    u8 bytes[4];
+    u32 word;
+} ScarabValues;
+
+/*0x20*/ extern ScarabValues _data_20;
+
+// Enables the Scarab UI counter upon collection
+RECOMP_PATCH void dll_297_func_1284(Object* self, Object* player, ScarabState* state) {
+  ScarabValues values = _data_20;
+
+  //@recomp: enable Scarab counter UI
+  set_gplay_bitstring(0x919, 1);
+    
+  ((DLL_Unknown*)player->dll)->vtbl->func[19].withTwoArgs((s32) player, values.bytes[state->unk29]);
+    
+  state->unk14 = 0x50;
+  state->unk18 = 0;
+}
