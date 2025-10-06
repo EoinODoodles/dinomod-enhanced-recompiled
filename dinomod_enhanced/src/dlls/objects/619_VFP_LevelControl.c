ce00e51a74bbc935dd5ffeb04b511cfe5864901c
Ethan Lafrenais <ethan.lafrenais@gmail.com>
2025-10-06 16:27:11 -0400

    Volcano Force Point patches (#13)

---
+++ dinomod_enhanced/src/dlls/objects/619_VFP_LevelControl.c
@@ -0,0 +1,124 @@
+#include "modding.h"
+
+#include "PR/ultratypes.h"
+#include "dlls/objects/210_player.h"
+#include "game/objects/object.h"
+#include "sys/main.h"
+#include "sys/objects.h"
+#include "sys/objtype.h"
+#include "sys/print.h"
+#include "dll.h"
+#include "functions.h"
+
+#include "recomp/dlls/objects/619_VFP_LevelControl_recomp.h"
+
+extern void VFP_LevelControl_func_8EC(Object *self);
+extern void VFP_LevelControl_func_A08(Object *self);
+extern void VFP_LevelControl_func_AAC(Object *self);
+
+RECOMP_PATCH void VFP_LevelControl_create(Object* self, ObjCreateInfo* createInfo, s32 a2) {
+    u8 mapSetupID;
+
+    obj_add_object_type(self, 0xA);
+    // @recomp: Don't force act 1
+    //gDLL_29_Gplay->vtbl->set_map_setup((s32) self->mapID, 1);
+    mapSetupID = gDLL_29_Gplay->vtbl->get_map_setup((s32) self->mapID);
+    switch (mapSetupID) {
+    case 1:
+    case 2:
+        break;
+    case 0:
+        func_80000860(self, self, 0x105, 0);
+        func_80000860(self, self, 0x106, 0);
+        func_80000860(self, self, 0x107, 0);
+        break;
+    case 3:
+        func_80000860(self, self, 0x105, 0);
+        func_80000860(self, self, 0x106, 0);
+        func_80000860(self, self, 0x107, 0);
+        func_80000450(self, self, 0x166, 0, 0, 0);
+        func_80000450(self, self, 0x167, 0, 0, 0);
+        func_80000450(self, self, 0x168, 0, 0, 0);
+        func_80000450(self, self, 0x169, 0, 0, 0);
+        func_80000450(self, self, 0x174, 0, 0, 0);
+        break;
+    }
+    self->unk0xb0 |= 0x6000;
+}
+
+/*0x0*/ extern s16 _data_0;
+
+RECOMP_PATCH void VFP_LevelControl_update(Object* self) {
+    Object* player;
+    u8 mapSetupID;
+
+    player = get_player();
+    map_get_map_id_from_xz_ws(player->srt.transl.x, player->srt.transl.z);
+    diPrintf("ACT %d \n", gDLL_29_Gplay->vtbl->get_map_setup(self->mapID));
+    mapSetupID = gDLL_29_Gplay->vtbl->get_map_setup(self->mapID);
+    switch (mapSetupID) {
+    case 1:
+        if (_data_0 != 0) {
+            _data_0 -= (s16)delayFloat;
+            if (_data_0 <= 0) {
+                _data_0 = 0;
+                func_80000860(self, self, 0x105, 0);
+                func_80000860(self, self, 0x106, 0);
+                func_80000860(self, self, 0x107, 0);
+                func_80000450(self, self, 0x166, 0, 0, 0);
+                func_80000450(self, self, 0x167, 0, 0, 0);
+                func_80000450(self, self, 0x168, 0, 0, 0);
+                func_80000450(self, self, 0x169, 0, 0, 0);
+                func_80000450(self, self, 0x174, 0, 0, 0);
+                func_80000450(self, self, 0x178, 0, 0, 0);
+                // @recomp: Don't give SpellStone back (breaks progression)
+                //set_gplay_bitstring(0x123, 1); // spellstone 1
+            }
+        }
+        VFP_LevelControl_func_8EC(self);
+    case 0:
+        return;
+    case 2:
+        if (_data_0 != 0) {
+            _data_0 -= (s16)delayFloat;
+            if (_data_0 <= 0) {
+                _data_0 = 0;
+                set_gplay_bitstring(0x4E9, 1);
+                func_80000860(self, self, 0x105, 0);
+                func_80000860(self, self, 0x106, 0);
+                func_80000860(self, self, 0x107, 0);
+                func_80000450(self, self, 0x166, 0, 0, 0);
+                func_80000450(self, self, 0x167, 0, 0, 0);
+                func_80000450(self, self, 0x168, 0, 0, 0);
+                func_80000450(self, self, 0x169, 0, 0, 0);
+                func_80000450(self, self, 0x174, 0, 0, 0);
+                func_80000450(self, self, 0x178, 0, 0, 0);
+                // @recomp: Don't give SpellStone back (breaks progression)
+                //set_gplay_bitstring(0x83B, 1); // spellstone 2
+                set_gplay_bitstring(0x22B, 1);
+            }
+        }
+        VFP_LevelControl_func_A08(self);
+        break;
+    case 3:
+        if (_data_0 != 0) {
+            _data_0 -= (s16)delayFloat;
+            if (_data_0 <= 0) {
+                _data_0 = 0;
+                set_gplay_bitstring(0x4E9, 1);
+                set_gplay_bitstring(0x63C, 1);
+                func_80000860(self, self, 0x105, 0);
+                func_80000860(self, self, 0x106, 0);
+                func_80000860(self, self, 0x107, 0);
+                func_80000450(self, self, 0x166, 0, 0, 0);
+                func_80000450(self, self, 0x167, 0, 0, 0);
+                func_80000450(self, self, 0x168, 0, 0, 0);
+                func_80000450(self, self, 0x169, 0, 0, 0);
+                func_80000450(self, self, 0x174, 0, 0, 0);
+                set_gplay_bitstring(0x83C, 1);
+            }
+        }
+        VFP_LevelControl_func_AAC(self);
+        break;
+    }
+}
---
+++ dinomod_enhanced/src/dlls/objects/633_VFP_PodiumPoint.c
@@ -0,0 +1,35 @@
+#include "modding.h"
+
+#include "PR/ultratypes.h"
+#include "game/objects/object.h"
+#include "recomputils.h"
+#include "sys/main.h"
+#include "sys/objects.h"
+#include "dll.h"
+
+#include "recomp/dlls/objects/633_VFP_PodiumPoint_recomp.h"
+
+extern s32 data_0;
+
+extern void VFP_PodiumPoint_func_1B8(Object* self);
+
+RECOMP_PATCH void VFP_PodiumPoint_update(Object* self) {
+    u8 mapSetupID;
+
+    mapSetupID = gDLL_29_Gplay->vtbl->get_map_setup(self->mapID);
+    switch (mapSetupID) {
+    default:
+        data_0 = 0x123;
+        break;
+    case 1:
+        data_0 = 0x22B; // @recomp: Accept DIM's activated SpellStone instead of the unactivated one
+        break;
+    case 2:
+        data_0 = 0x83B;
+        break;
+    case 3:
+        data_0 = 0x83C;
+        break;
+    }
+    VFP_PodiumPoint_func_1B8(self);
+}
