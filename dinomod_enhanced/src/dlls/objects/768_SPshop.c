6e8eec0e121b30d8937d65e9a5f70c7911e0d670
EoinODoodles <e.odrisceoil@gmail.com>
2025-10-14 21:10:26 +0100

    Added a fade-to-black when leaving shop

--- dinomod_enhanced/src/dlls/objects/768_SPshop.c
+++ dinomod_enhanced/src/dlls/objects/768_SPshop.c
@@ -24,6 +24,17 @@
 
 extern ShopItem shopItemData[];
 
+typedef struct {
+    s8 unk0;
+    u8 itemIndex;
+    s8 unk2;
+    s8 unk3;
+    s8 unk4;
+    //@recomp: extended struct
+    s32 transitionTimer;    //frames until player is warped away when leaving shop
+    s8 doWarp;              //Boolean tracking whether the exit transition has started
+} Extended_SPShop_Data;
+
 /** Shop item data edits (originally by jeebs2kx) 
     Allows Krystal and Sabre's Lanterns to be purchased separately
     Allows Sabre's Small Food Bag to be purchased
@@ -40,4 +51,66 @@ RECOMP_HOOK_DLL(SPShop_ctor) void hook_edit_shop_item_data() {
     //Krazoa Translator (repurposing Large Player Foodbag slot)
     shopItemData[SPItem_Large_Player_Foodbag].krystal.show = BIT_ALWAYS_1;
     shopItemData[SPItem_Large_Player_Foodbag].krystal.hide = BIT_7BF;
+}
+
+/** Adds a fade-to-black when leaving the shop (Banjeoin) */
+RECOMP_PATCH void SPShop_control(Object* self) {
+    Extended_SPShop_Data* objData; 
+    Object* player;
+
+    objData = self->data; //@recomp: get objData
+    player = get_player();
+
+    if (self->unk0xdc == 0) {
+        gDLL_29_Gplay->vtbl->set_obj_group_status(self->mapID, 0, 1);
+        gDLL_29_Gplay->vtbl->set_obj_group_status(self->mapID, 5, 1);
+
+        if (player->id == OBJ_Krystal) {
+            gDLL_29_Gplay->vtbl->set_obj_group_status(self->mapID, 6, 1);
+        } else {
+            gDLL_29_Gplay->vtbl->set_obj_group_status(self->mapID, 7, 1);
+        }
+
+        func_80000860(self, self, 0x1C8, 0);
+        func_80000860(self, self, 0x1CB, 0);
+        func_80000450(self, self, 0x22F, 0, 0, 0);
+        func_80000450(self, self, 0x231, 0, 0, 0);
+        main_set_bits(BIT_SP_Entered_Shop, 1);
+        gDLL_5_AMSEQ2->vtbl->func0(NULL, 0xF3, 0, 0, 0);
+        func_8001EBD0(1);
+        self->unk0xdc = 1;
+    }
+
+    //@recomp: update transition timer
+    if (objData->doWarp) {
+        objData->transitionTimer -= delayByte;
+        if (objData->transitionTimer <= 0){
+            objData->transitionTimer = 0;
+        }
+    }
+
+    //@recomp: instead of immediately warping away when leaving shop, fade to black then warp
+    if (!objData->doWarp && main_get_bits(BIT_SP_Exiting_Shop)) {
+        gDLL_28_ScreenFade->vtbl->fade(20, SCREEN_FADE_BLACK);
+        objData->transitionTimer = 35;
+        objData->doWarp = TRUE;
+        //TO-DO: lock player control until warp is finished
+    }
+
+    //@recomp: handle warping away once transition timer finished
+    if (objData->doWarp && objData->transitionTimer <= 0) {
+        //Fade from black after warping
+        gDLL_28_ScreenFade->vtbl->fade_reversed(20, SCREEN_FADE_BLACK);
+
+        if (player->id == OBJ_Sabre) {
+            warpPlayer(0xF, FALSE);
+        } else {
+            warpPlayer(0xE, FALSE);
+        }
+    }
+}
+
+//@recomp: extend object's Data struct
+RECOMP_PATCH u32 SPShop_get_data_size(Object *self, u32 a1) {
+    return sizeof(Extended_SPShop_Data);
 }
