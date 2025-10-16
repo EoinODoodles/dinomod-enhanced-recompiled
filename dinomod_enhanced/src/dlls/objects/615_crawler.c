d7386d819ec1256254061c868d23ef70ff7418c3
EoinODoodles <e.odrisceoil@gmail.com>
2025-10-16 15:07:59 +0100

    Ported MusicalProgrammer's Skeetla malloc free fix

---
+++ dinomod_enhanced/src/dlls/objects/615_crawler.c
@@ -0,0 +1,19 @@
+#include "modding.h"
+
+#include "common.h"
+#include "sys/objanim.h"
+#include "sys/objtype.h"
+
+#include "recomp/dlls/objects/615_crawler_recomp.h"
+
+//Removes an if condition that prevents memory being freed (originally by MusicalProgrammer)
+RECOMP_PATCH void dll_615_func_1AE8(Object* self) {
+    //@recomp: remove ID condition and always free object
+    func_800267A4(self);
+    obj_destroy_object(self);
+    
+    obj_free_tick(self);
+    func_800267A4(self);
+    obj_free_object_type(self, 4);    
+    self->srt.flags |= 0x4000;
+}
