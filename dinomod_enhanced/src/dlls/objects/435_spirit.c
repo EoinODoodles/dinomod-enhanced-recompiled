47b4bccfbc23a4c7460a55e391799e2b3702558b
EoinODoodles <e.odrisceoil@gmail.com>
2025-11-24 19:59:20 +0000

    Spirit: setup patch, and use recomp symbols

--- dinomod_enhanced/src/dlls/objects/435_spirit.c
+++ dinomod_enhanced/src/dlls/objects/435_spirit.c
@@ -3,18 +3,71 @@
 
 #include "common.h"
 
-#include "recomp/dlls/_asm/435_recomp.h"
+#include "recomp/dlls/objects/435_KrazoaSpirit_recomp.h"
+#include "segment_334F0.h"
+#include "sys/math.h"
+#include "sys/print.h"
+
+typedef struct {
+    AnimObj_Setup base;
+} Spirit_Setup;
 
 typedef struct {
     AnimObj_Data base;
     /** RECOMP EXTENDED */
+    TextureAnimator* animTexture;
     u32 frameTimer;
 } Spirit_Data;
 
-extern DLL_Unknown* _data_0;
+extern DLL_Unknown* data_modGfx;
+
+/** Getting the texture animator so it can be used in the control function */
+RECOMP_PATCH void Spirit_setup(Object* self, Spirit_Setup* objSetup, s32 arg2) {
+    Spirit_Data* objData;
+    s32 temp_v0;
+    s16 id;
+
+    objData = self->data;
+    
+    objData->base.unk76 = objSetup->base.unk1A;
+    objData->base.unk7A = -1;
+    objData->base.unk24 = 1.0f / ((u8)objSetup->base.unk24 + 1.0f);
+    objData->base.unk28 = -1;
+    
+    temp_v0 = self->unkDC;
+    
+    if ((temp_v0 == 0) && (objSetup->base.sequenceIdBitfield != 1)) {
+        gDLL_3_Animation->vtbl->func6(objData, objSetup);
+        self->unkDC = objSetup->base.sequenceIdBitfield + 1;
+    } else if (temp_v0 && (temp_v0 != objSetup->base.sequenceIdBitfield + 1)) {
+        gDLL_3_Animation->vtbl->func8(objData);
+        if (objSetup->base.sequenceIdBitfield != -1) {
+            gDLL_3_Animation->vtbl->func6(objData, objSetup);
+        }
+        self->unkDC = objSetup->base.sequenceIdBitfield + 1;
+    }
+    
+    id = self->id;
+    self->opacityWithFade = 0;
+    self->opacity = 0;
+    
+    if (id == OBJ_DBSH_SpiritPriz || 
+        id == OBJ_DFSH_SpiritPriz || 
+        id == OBJ_MMSH_SpiritPriz || 
+        id == OBJ_ECSH_SpiritPriz || 
+        id == OBJ_GPSH_SpiritPriz || 
+        id == OBJ_CCSH_SpiritPriz || 
+        id == OBJ_WGSH_SpiritPriz || 
+        id == OBJ_NWSH_SpiritPriz) {   
+        data_modGfx = dll_load_deferred(0x102D, 1);
+    }
+
+    //@recomp: get texture animator
+    objData->animTexture = func_800348A0(self, 0, 0);
+}
 
 /** Play the Krazoa Spirits' unused trail texture animation */
-RECOMP_PATCH void dll_435_print(Object* self, Gfx **gdl, Mtx **mtxs, Vertex **vtxs, Triangle **pols, s8 visibility) {
+RECOMP_PATCH void Spirit_print(Object* self, Gfx **gdl, Mtx **mtxs, Vertex **vtxs, Triangle **pols, s8 visibility) {
     s16 id;
     s16 random;
     Camera *camera;
@@ -41,11 +94,10 @@ RECOMP_PATCH void dll_435_print(Object* self, Gfx **gdl, Mtx **mtxs, Vertex **vt
         id == OBJ_NWSH_Spirit
     ){
         objData->frameTimer += gUpdateRate;
-        objData->frameTimer %= 28 * 2;
+        objData->frameTimer %= 28 * 2; //28 frames
 
-        animatedTexture = func_800348A0(self, 0, 0);
-        if (animatedTexture){
-            animatedTexture->unk0 = (objData->frameTimer / 2) << 8;
+        if (objData->animTexture){
+            objData->animTexture->unk0 = (objData->frameTimer / 2) << 8;
         }
     }
 
@@ -54,25 +106,25 @@ RECOMP_PATCH void dll_435_print(Object* self, Gfx **gdl, Mtx **mtxs, Vertex **vt
         if (self->opacity) {
             if (id == OBJ_DFSH_SpiritPriz || id == OBJ_CCSH_SpiritPriz) {
                 if ((s16)rand_next(0, 1)) {
-                    _data_0->vtbl->func[0].withSixArgs((s32)self, 0, 0, 4, -1, 0);
+                    data_modGfx->vtbl->func[0].withSixArgs((s32)self, 0, 0, 4, -1, 0);
                 }
             } else if (id == OBJ_MMSH_SpiritPriz || id == OBJ_WGSH_SpiritPriz) {
                 if ((s16)rand_next(0, 1)) {
-                    _data_0->vtbl->func[0].withSixArgs((s32)self, 1, 0, 4, -1, 0);
+                    data_modGfx->vtbl->func[0].withSixArgs((s32)self, 1, 0, 4, -1, 0);
                 }
             } else if (id == OBJ_ECSH_SpiritPriz || id == OBJ_GPSH_SpiritPriz) {
                 if ((s16)rand_next(0, 1)) {
-                    _data_0->vtbl->func[0].withSixArgs((s32)self, 2, 0, 4, -1, 0);
+                    data_modGfx->vtbl->func[0].withSixArgs((s32)self, 2, 0, 4, -1, 0);
                 }
             } else if (id == OBJ_DBSH_SpiritPriz || id == OBJ_NWSH_SpiritPriz){
                 if ((s16)rand_next(0, 1)) {
-                    _data_0->vtbl->func[0].withSixArgs((s32)self, 3, 0, 4, -1, 0);
+                    data_modGfx->vtbl->func[0].withSixArgs((s32)self, 3, 0, 4, -1, 0);
                 }
             }
         }
     }
 }
 
-RECOMP_PATCH u32 dll_435_get_data_size(Object *self, u32 a1) {
+RECOMP_PATCH u32 Spirit_get_data_size(Object *self, u32 a1) {
     return sizeof(Spirit_Data);
 }
