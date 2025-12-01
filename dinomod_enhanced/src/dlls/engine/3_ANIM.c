a666545c470ceb303e3a686b41b8bd20b00ac8fc
Ethan Lafrenais <ethan.lafrenais@gmail.com>
2025-12-01 00:08:44 -0500

    Prevent anim export 17 from running if the given object seq idx is out of bounds
    - Indirectly fixes the skeetla seq issue
    - Prevents the anim seq slots from filling up (the vanilla func has a bug in this case)

--- dinomod_enhanced/src/dlls/engine/3_ANIM.c
+++ dinomod_enhanced/src/dlls/engine/3_ANIM.c
@@ -5,9 +5,9 @@
 
 #include "recomp/dlls/engine/3_ANIM_recomp.h"
 
-typedef void (*Export17)(s32 objectSeqIndex, Object* object, s32 arg2);
+typedef s32 (*Export17)(s32 objectSeqIndex, Object* object, s32 arg2);
 static Export17 export17_func; 
-static void export17_hijack(s32 objectSeqIndex, Object* object, s32 arg2);
+static s32 export17_hijack(s32 objectSeqIndex, Object* object, s32 arg2);
 
 RECOMP_HOOK_DLL(dll_3_ctor) void anim_ctor_hook(DLLFile *dll) {
     export17_func = dinomod_hijack_dll_export(dll, 17, export17_hijack);
@@ -17,13 +17,20 @@ RECOMP_HOOK_RETURN_DLL(dll_3_dtor) void anim_dtor_hook() {
     export17_func = NULL;
 }
 
-static void export17_hijack(s32 objectSeqIndex, Object* object, s32 arg2) {
-    // @recomp: Ignore if the ObjDef sequence ID is -1
-    if (object->def != NULL && object->def->pSeq != NULL) {
+static s32 export17_hijack(s32 objectSeqIndex, Object* object, s32 arg2) {
+    if (object->def != NULL) {
+        // @recomp: Ignore if the given index is out of bounds. The actual function has a bug in this
+        //          specific case where it will acquire a sequence slot but never free it when it sees
+        //          that the index is out of bounds. Over time, this will make it impossible for any
+        //          object sequence to play for the rest of the session.
+        if (objectSeqIndex < 0 || objectSeqIndex >= object->def->numSequences || object->def->pSeq == NULL) {
+            return -1;
+        }
+        // @recomp: Ignore if the ObjDef sequence ID is -1. This is new behavior required by dinomod.
         if (object->def->pSeq[objectSeqIndex] == -1) {
-            return;
+            return -1;
         }
     }
 
-    export17_func(objectSeqIndex, object, arg2);
+    return export17_func(objectSeqIndex, object, arg2);
 }
