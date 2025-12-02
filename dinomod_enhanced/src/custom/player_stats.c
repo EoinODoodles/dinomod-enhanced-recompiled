871968ec5b8fc07eabe534c3e321f646a5ea6463
EoinODoodles <e.odrisceoil@gmail.com>
2025-12-02 14:53:37 +0000

    player_stats: use Gamebit enum where possible, cleanup

--- dinomod_enhanced/src/custom/player_stats.c
+++ dinomod_enhanced/src/custom/player_stats.c
@@ -1,36 +1,37 @@
 /** Helper functions for getting player's SpellStone/Spirit/Duster counts */
 
 #include "player_stats.h"
+#include "game/gamebits.h"
 #include "sys/main.h"
 #include "sys/print.h"
 
-enum GameBits{
-    FLAG_WARLOCK_MOUNTAIN_SETUP = 0xe4,
+typedef enum {
+    FLAG_WARLOCK_MOUNTAIN_SETUP = BIT_WM_Map_Setup,
 
-    FLAG_SPELLSTONE_DIM_COLLECTED = 0x123,
-    FLAG_SPELLSTONE_DIM_ACTIVATED = 0x22b,
+    FLAG_SPELLSTONE_DIM_COLLECTED = BIT_SpellStone_DIM,
+    FLAG_SPELLSTONE_DIM_ACTIVATED = BIT_SpellStone_DIM_Activated,
     FLAG_SPELLSTONE_DIM_DEPOSITED = 0x877,
 
     FLAG_SPELLSTONE_CRF_COLLECTED = NULL,
-    FLAG_SPELLSTONE_CRF_ACTIVATED = 0x2e8,
-    FLAG_SPELLSTONE_CRF_DEPOSITED = 0x5f3,
+    FLAG_SPELLSTONE_CRF_ACTIVATED = BIT_SpellStone_CRF,
+    FLAG_SPELLSTONE_CRF_DEPOSITED = BIT_DFP_Place_Spellstone_One,
 
     FLAG_SPELLSTONE_WC_COLLECTED = NULL,
-    FLAG_SPELLSTONE_WC_ACTIVATED = 0x83b,
+    FLAG_SPELLSTONE_WC_ACTIVATED = BIT_SpellStone_WC,
     FLAG_SPELLSTONE_WC_DEPOSITED = 0x878,
 
     FLAG_SPELLSTONE_BWC_COLLECTED = NULL,
-    FLAG_SPELLSTONE_BWC_ACTIVATED = 0x83a,
-    FLAG_SPELLSTONE_BWC_DEPOSITED = 0x5f4,
+    FLAG_SPELLSTONE_BWC_ACTIVATED = BIT_SpellStone_BWC,
+    FLAG_SPELLSTONE_BWC_DEPOSITED = 0x5F4,
 
     FLAG_SPELLSTONE_KP_COLLECTED = NULL,
-    FLAG_SPELLSTONE_KP_ACTIVATED = 0x7bd,
+    FLAG_SPELLSTONE_KP_ACTIVATED = BIT_SpellStone_KP,
     FLAG_SPELLSTONE_KP_DEPOSITED = 0x5f5,
 
     FLAG_SPELLSTONE_DR_COLLECTED = NULL,
     FLAG_SPELLSTONE_DR_ACTIVATED = 0x7cc,
     FLAG_SPELLSTONE_DR_DEPOSITED = 0x8fa,
-};
+} SpellStoneSpiritGamebits;
 
 const int spellStoneFlags[6][3] = {
     {
@@ -82,8 +83,6 @@ s8 getCountSpellStones(){
         }
     }
 
-    // diPrintf("Spellstones: %1d\n", spellStones);
-
     return spellStones;
 }
 
@@ -91,12 +90,11 @@ s8 getCountSpirits(){
     s8 spirits = 0;
 
     spirits = main_get_bits(FLAG_WARLOCK_MOUNTAIN_SETUP) - 1;
-    if (spirits < 0)
+    if (spirits < 0){
         spirits = 0;
-    if (spirits > 8)
+    } else if (spirits > 8){
         spirits = 8;
-
-    // diPrintf("Spirits: %1d\n", spirits);
+    }
 
     return spirits;
 }
