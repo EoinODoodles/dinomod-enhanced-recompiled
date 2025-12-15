9e940af22b1e44a614fe5ebbd9a80b015c53ff38
EoinODoodles <e.odrisceoil@gmail.com>
2025-12-15 19:58:42 +0000

    SHTricky: prevent crash

--- dinomod_enhanced/src/dlls/objects/501_SHtricky.c
+++ dinomod_enhanced/src/dlls/objects/501_SHtricky.c
@@ -20,7 +20,7 @@ RECOMP_PATCH void SHtricky_setup(Object* self, ObjSetup* setup, s32 arg2) {
     objdata = (SHtricky_Data*)self->data;
     
     // @recomp: Stop Tricky from repeating: "That's my mom!" when arriving in SwapStone Hollow (originally by MusicalProgrammer)
-    if (main_get_bits(0xA1) != 0) {
+    if (main_get_bits(BIT_A1)) {
         objdata->unk0 = 3;
     } else {
         main_set_bits(BIT_Play_Seq_00D1, 0);
@@ -28,4 +28,34 @@ RECOMP_PATCH void SHtricky_setup(Object* self, ObjSetup* setup, s32 arg2) {
     }
 
     self->unkB0 |= 0x2000;
+}
+
+RECOMP_PATCH void SHtricky_control(Object *self) {
+    SHtricky_Data *objdata;
+    Object *sidekick;
+
+    objdata = (SHtricky_Data*)self->data;
+    sidekick = get_sidekick();
+    //@recomp: prevent crash if Tricky isn't loaded
+    if (sidekick == NULL){
+        return;
+    }
+
+    switch (objdata->unk0) {
+    case 0:
+        if (main_get_bits(BIT_Play_Seq_00D1)) {
+            gDLL_3_Animation->vtbl->func17(3, sidekick, -1);
+            objdata->unk0 = 1;
+        } 
+        break;
+    case 1:
+        objdata->unk0 = 2;
+        break;
+    case 2:
+        ((DLL_ISidekick*)sidekick->dll)->vtbl->func22(sidekick, self);
+        objdata->unk0 = 3;
+        break;
+    case 3:
+        break;
+    }
 }
