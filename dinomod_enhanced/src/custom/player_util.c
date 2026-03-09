bee13fc2f832030ba0493c2179576a654437447f
EoinODoodles <e.odrisceoil@gmail.com>
2026-03-09 19:50:20 +0000

    SCbeacon/Tumbleweed fixes

---
+++ dinomod_enhanced/src/custom/player_util.c
@@ -0,0 +1,71 @@
+#include "player_util.h"
+#include "dll.h"
+#include "dlls/engine/18_objfsa.h"
+#include "recomputils.h"
+
+#include "game/objects/object_id.h"
+#include "sys/objects.h"
+#include "dlls/objects/210_player.h"
+
+#include "recomp/dlls/objects/210_player_recomp.h"
+
+extern s16 _data_98[];
+extern f32 _data_6F8;
+
+/** Switches the player back to their basic walking animations (weapon stowed) */
+void playerUtil_use_walk_anims(Object* player){
+    Player_Data* objData;
+    if (!player){
+        return;
+    }
+
+    objData = player->data;
+    if (!objData){
+        return;
+    }
+
+    objData->unk3C4 = &_data_6F8;
+    objData->modAnims = _data_98;
+}
+
+/** Forces the player out of carrying an Object, and into their default standing state */
+void playerUtil_stop_carrying(Object* player){
+    Player_Data* objData;
+    if (!player){
+        return;
+    }
+
+    objData = player->data;
+    if (!objData){
+        return;
+    }
+
+    objData->unk870 = 0;
+    objData->unk868 = NULL;
+
+    playerUtil_use_walk_anims(player);
+
+    gDLL_18_objfsa->vtbl->set_anim_state(player, &objData->unk0, PLAYER_ASTATE_Standing);
+}
+
+/**
+  * Returns TRUE if the player is currently walking/standing/turning, or FALSE if they're in some other animState
+  */
+int playerUtil_is_player_standing_or_walking(Object* player){
+    Player_Data* objData;
+    ObjFSA_Data* fsa;
+    if (!player){
+        return FALSE;
+    }
+
+    objData = player->data;
+    if (!objData){
+        return FALSE;
+    }
+
+    fsa = &objData->unk0;
+
+    return (fsa->animState == PLAYER_ASTATE_Standing || 
+            fsa->animState == PLAYER_ASTATE_Turning_On_Spot || 
+            fsa->animState == PLAYER_ASTATE_Walking);
+}
