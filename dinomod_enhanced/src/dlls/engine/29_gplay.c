3263661290fe937602260368222b2c2434a63bee
EoinODoodles <e.odrisceoil@gmail.com>
2026-04-19 19:06:23 +0100

    gplay cleanup

--- dinomod_enhanced/src/dlls/engine/29_gplay.c
+++ dinomod_enhanced/src/dlls/engine/29_gplay.c
@@ -2,9 +2,11 @@
 #include "recomputils.h"
 
 #include "PR/ultratypes.h"
+#include "game/gamebits.h"
 #include "sys/map_enums.h"
 #include "sys/main.h"
 #include "dll.h"
+#include "dlls/engine/6_amsfx.h"
 
 #include "recomp/dlls/engine/29_gplay_recomp.h"
 
@@ -14,8 +16,8 @@ extern GplayOptions *sGameOptions;
 
 /** Modifies the flagIDs used to track maps' objectGroup load states (originally by MusicalProgrammer) */
 RECOMP_HOOK_DLL(gplay_ctor) void gplay_patch_map_object_group_flags() {
-    sMapObjGroupBitKeys[MAP_EARTHWALKER_TEMPLE] = 0x36a; //Shares Walled City's flag
-    sMapObjGroupBitKeys[MAP_BOSS_KAMERIAN_DRAGON] = 0x5dc; //Shares the same flag as the rest of Dragon Rock (Bottom)
+    sMapObjGroupBitKeys[MAP_EARTHWALKER_TEMPLE] = BIT_WC_ObjGroup_Bits; //Shares Walled City's gamebit
+    sMapObjGroupBitKeys[MAP_BOSS_KAMERIAN_DRAGON] = BIT_DR_Bottom_ObjGroup_Bits; //Shares the same gamebit as the rest of Dragon Rock (Bottom)
 }
 
 /** Checks if the Scarab collection cutscene has already played, and if so unlocks the Scarab UI
@@ -23,8 +25,8 @@ RECOMP_HOOK_DLL(gplay_ctor) void gplay_patch_map_object_group_flags() {
   *  This is mostly intended for players who start out playing unmodded, since it'll put the Scarab UI into its correct state) 
   */
 RECOMP_HOOK_RETURN_DLL(gplay_start_game) void dll_gplay_hook_enable_scarabs_if_already_collected() {
-    if (main_get_bits(0x910) && main_get_bits(0x919) == 0){
-        main_set_bits(0x919, 1);
+    if (main_get_bits(BIT_Tutorial_Collected_Scarab) && main_get_bits(BIT_919) == FALSE){
+        main_set_bits(BIT_919, TRUE);
     }
 }
 
@@ -45,8 +47,8 @@ RECOMP_PATCH u32 gplay_load_game_options(void) {
         // "gplayLoadOptions error: saveoptions failed to load.\n" (default.dol)
         bzero(sGameOptions, sizeof(GplayOptions));
         ret = 0;
-        sGameOptions->volumeMusic = 0x7f;
-        sGameOptions->volumeAudio = 0x7f;
+        sGameOptions->volumeMusic = MAX_VOLUME;
+        sGameOptions->volumeAudio = MAX_VOLUME;
         sGameOptions->unkA = 0x7f;
     }
 
