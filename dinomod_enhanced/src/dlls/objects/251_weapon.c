36e15181bb56c45d8a346866b5d72e973ff5e49d
EoinODoodles <e.odrisceoil@gmail.com>
2026-04-28 18:01:11 +0100

    Weapon: indirect port of MusicalProgrammer's transparent shape collision fix

---
+++ dinomod_enhanced/src/dlls/objects/251_weapon.c
@@ -0,0 +1,24 @@
+#include "modding.h"
+#include "recompconfig.h"
+#include "recomputils.h"
+
+#include "common.h"
+#include "game/gamebits.h"
+#include "game/objects/object_id.h"
+#include "sys/gfx/modgfx.h"
+#include "sys/main.h"
+#include "sys/objects.h"
+
+#include "recomp/dlls/_asm/251_recomp.h"
+
+/** 
+  * Change collision bits for weapons, allowing them to phase through
+  * light beams instead of striking them (originally by MusicalProgrammer)
+  *
+  * TODO: patch the constructor function instead once it's decomped?
+  */
+RECOMP_HOOK_DLL(dll_251_control) void weapon_phase_through_transparent_shapes(Object* self) {
+    if ((self->objhitInfo) && (self->objhitInfo->unkA1 != 1)) {
+        self->objhitInfo->unkA1 = 1;
+    }
+}
