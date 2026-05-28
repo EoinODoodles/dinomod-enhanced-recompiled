48f6b953eea48929ef95bb0f017a99e1cd1d4c26
EoinODoodles <e.odrisceoil@gmail.com>
2026-05-28 14:52:49 +0100

    Cmdmenu: sidekick icon/meter bugfixes (should help prevent defaulting to Tricky icon, and ensure meter doesn't pop in at full opacity)

--- dinomod_enhanced/src/dlls/engine/1_cmdmenu.c
+++ dinomod_enhanced/src/dlls/engine/1_cmdmenu.c
@@ -2824,7 +2824,7 @@ static void cmdmenu_draw_main_custom(Gfx** gdl, Mtx** mtxs, Vertex** vtxs) {
     s32 sideCommandIndex;
     s32 tileCount;
     u8 iconOpacity;
-    u8 pageIcon;
+    u8 pageIcon = 0;
     s8 offsetX;
     s8 offsetY;
     Object* sidekick;
@@ -3211,7 +3211,7 @@ static void cmdmenu_draw_main_custom(Gfx** gdl, Mtx** mtxs, Vertex** vtxs) {
 
         //Get page icon (Bag/SpellBook/Kyte/Tricky)
         if (dInventoryShow || 
-            dInventoryOpacity > 0 || //@recomp: stop sidekick icon appearing halfway through fading out bag/book icon 
+            dInventoryOpacity == MAX_OPACITY || 
             (dInventoryOpacity != 0 && dOpacitySidekickMeter == 0)
         ) {
             switch (sInventoryPageID) {
@@ -3247,14 +3247,14 @@ static void cmdmenu_draw_main_custom(Gfx** gdl, Mtx** mtxs, Vertex** vtxs) {
             }
         } else {
             //Show sidekick's icon when the sidekick meter should be visible
-            //@bug: can suddenly switch to sidekick icon halfway through fading out from bag/book
-            if (dOpacitySidekickMeter != 0) {
-                pageIcon = CMDMENU_TEX_42_Tricky;
+            //@bug: can suddenly switch to sidekick icon halfway through fading out from bag
+            if ((dOpacitySidekickMeter != 0) && (sidekick != NULL)) {
                 if (sidekick != NULL && sidekick->id == OBJ_Kyte) {
                     pageIcon = CMDMENU_TEX_54_Kyte;
                     iconOpacity = dOpacitySidekickMeter;
                 } else {
                     offsetY = 3;
+                    pageIcon = CMDMENU_TEX_42_Tricky;
                     iconOpacity = dOpacitySidekickMeter;
                 }
             } else {
@@ -3263,7 +3263,7 @@ static void cmdmenu_draw_main_custom(Gfx** gdl, Mtx** mtxs, Vertex** vtxs) {
         }
 
         //Draw page icon
-        if (iconOpacity) {
+        if (iconOpacity && pageIcon) {
             dInventoryPageIcon = tex_load_deferred(dTextableIDs[pageIcon]);
             rcp_screen_full_write(
                 gdl, 
@@ -3581,9 +3581,10 @@ RECOMP_PATCH void cmdmenu_draw_c_buttons_and_sidekick_meter(Gfx** gdl, Mtx** mtx
     //(@recomp: animate opacity even when sidekick's missing, so it doesn't get stuck)
     {
         //Handle the meter's opacity
-        if (((sInventoryPageID == CMDMENU_PAGE_7_Sidekick_Tricky || sInventoryPageID == CMDMENU_PAGE_8_Sidekick_Kyte) && dInventoryOpacity) || 
-            (sStatsChangeTimers.sidekickBlueFood >= 0.0f) ||
-            cmdmenu_should_sidekick_meter_appear_over_food_items(sidekick) //@recomp: optionally show meter while relevant food selected on Items page
+        if (sidekick && sStats.sidekickMaxFood && //@recomp: don't fade in when sidekick is missing, or sidekick data not loaded
+            (((sInventoryPageID == CMDMENU_PAGE_7_Sidekick_Tricky || sInventoryPageID == CMDMENU_PAGE_8_Sidekick_Kyte) && dInventoryOpacity) || 
+            ((sStatsChangeTimers.sidekickBlueFood >= 0.0f)) ||
+            cmdmenu_should_sidekick_meter_appear_over_food_items(sidekick)) //@recomp: optionally show meter while relevant food selected on Items page
         ) {
             /* Fade in while the sidekick's inventory page is open, 
                or when the sidekick's blue food count has recently changed */
