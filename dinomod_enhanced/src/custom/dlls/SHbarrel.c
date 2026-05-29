8564d15fde42f2da1abaff5c0251ca601eb0b8f2
Ethan Lafrenais <ethan.lafrenais@gmail.com>
2026-05-29 04:41:35 -0400

    barrel physics/collision that doesn't suck

--- dinomod_enhanced/src/custom/dlls/SHbarrel.c
+++ dinomod_enhanced/src/custom/dlls/SHbarrel.c
@@ -1,7 +1,6 @@
 #include "SHbarrel.h"
 #include "custom_objsetups.h"
 
-#include "dlls/engine/27.h"
 #include "dlls/engine/54_pickup.h"
 #include "dlls/objects/210_player.h"
 #include "game/objects/interaction_arrow.h"
@@ -14,21 +13,24 @@
 #include "sys/objprint.h"
 #include "sys/objtype.h"
 #include "dll.h"
-#include "sys/print.h"
 
 typedef struct {
     Pickup pickup;
-    DLL27_Data collider;
+    struct {
+        Unk80027934 col;
+        AABBs32 aabb;
+        Vec3f trackPrevPos[1];
+        f32 hitsTestRadii[1];
+        Vec3f hitsPrevPos[1];
+    } collider;
+    u8 inDeepWater;
+    f32 bumpCooldown;
 } SHbarrel_Data;
 
 void SHbarrel_ctor(void *dll) { }
 void SHbarrel_dtor(void *dll) { }
 
 static void SHbarrel_setup(Object* self, SHBarrel_Setup* setup, s32 reset) {
-    static Vec3f colliderPoints[] = {VEC3F(0.0f, 0.0f, 0.0f)}; // must be static
-    f32 colliderRadii[] = {self->objhitInfo->unk52};
-    u8 colliderArg[] = {1};
-
     SHbarrel_Data* objdata = self->data;
 
     obj_add_object_type(self, OBJTYPE_Barrel);
@@ -38,8 +40,15 @@ static void SHbarrel_setup(Object* self, SHBarrel_Setup* setup, s32 reset) {
     gDLL_54_pickup->vtbl->setup(self, (Pickup*)self->data, 33);
     gDLL_54_pickup->vtbl->set_dont_save(self->data, TRUE);
 
-    gDLL_27->vtbl->init(&objdata->collider, DLL27FLAG_1 | DLL27FLAG_2 | DLL27FLAG_4 | DLL27FLAG_20 | DLL27FLAG_40000, 0, DLL27MODE_1);
-    gDLL_27->vtbl->setup_terrain_collider(&objdata->collider, 1, colliderPoints, colliderRadii, colliderArg);
+    // Collider setup
+    objdata->collider.col.unk54[0] = 1; // some radius? not the radius of the barrel
+    objdata->collider.col.unk50[0] = -1;
+    objdata->collider.col.unk40[0] = self->objhitInfo->unk52; // barrel radius
+    objdata->collider.hitsTestRadii[0] = self->objhitInfo->unk52;
+
+    // Collider init
+    objdata->collider.trackPrevPos[0] = self->globalPosition;
+    objdata->collider.hitsPrevPos[0] = self->globalPosition;
 }
 
 static void SHbarrel_drop_if_player_underwater(Object* self) {
@@ -54,60 +63,193 @@ static void SHbarrel_drop_if_player_underwater(Object* self) {
     }
 }
 
-static void SHbarrel_not_held_control(Object* self, SHbarrel_Data* objdata) {
-    diPrintf("water: %f", &objdata->collider.underwaterDist);
-    if (objdata->collider.underwaterDist > 0.0f && objdata->collider.waterY != -100000.0f) {
-        Object* player = get_player();
-        if (player != NULL) {
-            Vec3f vec;
-            VECTOR_SUBTRACT(self->globalPosition, player->globalPosition, vec);
-            vec.y = 0.0f;
-            f32 playerDist = VECTOR_MAGNITUDE(vec);
+static void SHbarrel_player_push(Object* self, SHbarrel_Data* objdata) {
+    Object* player = get_player();
+    if (player == NULL) {
+        return;
+    }
+    
+    if ((self->objhitInfo->unk58 & 8) && self->objhitInfo->unk0 == player) {
+        // Touched by player
 
-            const f32 pushRange = 22.0f;
-            const f32 pushForce = 2.0f;
+        // Get vec from player to barrel
+        Vec3f vec;
+        VECTOR_SUBTRACT(self->globalPosition, player->globalPosition, vec);
+        vec.y = 0.0f;
+        f32 playerDist = VECTOR_MAGNITUDE(vec);
+        VECTOR_DIVIDE_BY_SCALAR(vec, playerDist);
 
-            if (playerDist <= pushRange && playerDist != 0.0f) {
-                VECTOR_DIVIDE_BY_SCALAR(vec, playerDist);
-                VECTOR_MULTIPLY_BY_SCALAR(vec, (1.0f - (playerDist / pushRange)) * pushForce * gUpdateRateF);
+        // Push
+        const f32 pushForce = 1.5f;
+        f32 playerSpeed = VECTOR_MAGNITUDE(player->velocity);
+        VECTOR_MULTIPLY_BY_SCALAR(vec, MIN(playerSpeed, pushForce) * gUpdateRateF);
 
-                self->velocity.x += vec.x;
-                self->velocity.z += vec.z;
+        self->velocity.x += vec.x;
+        self->velocity.z += vec.z;
+
+        // Bump barrel vertically when pushed
+        if (self->velocity.y >= 0.0f && objdata->bumpCooldown <= 0.0f) {
+            self->velocity.y += 0.1f;
+            objdata->bumpCooldown = 30.0f;
+        }
+    }
+}
+
+static void SHbarrel_physics(Object* self, SHbarrel_Data* objdata) {
+    s32 count;
+    s32 i;
+    f32 delta;
+    f32 diffMagnitude;
+    Func_80057F1C_Struct** collisionInfo;
+    f32 minDiffMagnitude;
+    f32 yDiff;
+    s32 inWater = FALSE;
+
+    if (objdata->bumpCooldown > 0.0f) {
+        objdata->bumpCooldown -= gUpdateRateF;
+    }
+
+    // TODO: this is super framerate dependent...
+
+    objdata->inDeepWater = FALSE;
+
+    //Float on water
+    {
+        //Find the nearest water plane
+        count = func_80057F1C(self, self->srt.transl.x, self->srt.transl.y, self->srt.transl.z, &collisionInfo, 0, 0);
+        if (count) {
+            delta = 10000.0f;
+            s32 floorFound = FALSE;
+            f32 floorDist = 100.0f;
+            
+            for (i = 0; i < count; i++){
+                if (collisionInfo[i]->unk14 == 14) { //water only
+                    yDiff = self->srt.transl.y - collisionInfo[i]->unk0[0];
+                    if (yDiff >= 0) {
+                        diffMagnitude = yDiff;
+                    } else {
+                        diffMagnitude = -yDiff;
+                    }
+                    
+                    if (delta >= 0) {
+                        minDiffMagnitude = delta;
+                    } else {
+                        minDiffMagnitude = -delta;
+                    }
+                    
+                    if (diffMagnitude < minDiffMagnitude) {
+                        delta = yDiff;
+                    }
+                } else {
+                    if (!floorFound && collisionInfo[i]->unk0[0] < (self->srt.transl.y + 5.0f) && collisionInfo[i]->unk0[2] > 0.707f) {
+                        floorDist = self->globalPosition.y - collisionInfo[i]->unk0[0];
+                        floorFound = TRUE;
+                    }
+                }
+            }
+
+            //Correlate velocityY with barrel's depth underwater
+            if ((delta != 10000.0f) && (delta < 0)) {
+                const f32 offset = 5.0f;
+                self->velocity.y += (-(delta + offset) * 0.1f);
+                self->velocity.y *= 0.8f;
+
+                // Let player push barrel while in water
+                if (floorFound && floorDist > 0.0f) {
+                    SHbarrel_player_push(self, objdata);
+                    inWater = TRUE;
+                } else if (delta < -2.0f) {
+                    // Hit the floor but still reasonably underwater, don't count as beached
+                    inWater = TRUE;
+                }
+
+                if (floorDist > 25.0f) {
+                    objdata->inDeepWater = TRUE;
+                }
             }
         }
 
-        f32 targetY = objdata->collider.waterY - 7.0f;
-
-        self->velocity.y += (targetY - self->globalPosition.y) * 0.01f * gUpdateRateF;
-        self->velocity.y = CLAMP_EXPR(self->velocity.y, -20.0f, 20.f);
-
-        self->velocity.x *= 0.85f;
-        self->velocity.y *= 0.95f;
-        self->velocity.z *= 0.85f;
-
-        self->unkAF |= ARROW_FLAG_8_No_Targetting;
-
-        objdata->collider.mode = DLL27MODE_1;
-        gDLL_54_pickup->vtbl->set_gravity(self->data, FALSE);
-
-        obj_move(self, 
-            self->velocity.x * gUpdateRateF, 
-            self->velocity.y * gUpdateRateF, 
-            self->velocity.z * gUpdateRateF);
-    } else {
-        // self->velocity.x *= 0.80f;
-        // self->velocity.y -= 0.1f * gUpdateRateF;
-        // self->velocity.z *= 0.80f;
-        self->velocity.x = 0.0f;
-        //self->velocity.y = 0.0f;
-        self->velocity.z = 0.0f;
-        gDLL_54_pickup->vtbl->set_gravity(self->data, TRUE);
-
-        self->unkAF &= ~ARROW_FLAG_8_No_Targetting;
-        objdata->collider.mode = DLL27MODE_2;
+        // Damp xz velocity
+        if (inWater) {
+            self->velocity.x *= 0.85f;
+            self->velocity.z *= 0.85f;
+        } else {
+            self->velocity.x *= 0.5f;
+            self->velocity.z *= 0.5f;
+        }
     }
 
-    
+    obj_move(self, self->velocity.x, self->velocity.y, self->velocity.z);
+}
+
+static void SHbarrel_collision(Object* self, SHbarrel_Data* objdata, s32 pickupState) {
+    // Note: Collision detection assumes a single test point at localspace 0,0,0
+
+    if (pickupState == PICKUP_NotHeld) {
+        // Do hit line collision detection
+        Vec3f currPos[1] = {self->srt.transl};
+        func_80059C40(
+            &objdata->collider.hitsPrevPos[0], 
+            &currPos[0], 
+            objdata->collider.hitsTestRadii[0], 
+            0, 
+            NULL, 
+            self, 
+            /*config*/1, -1, 0, 0);
+        
+        // Do hit line collision resolution
+        self->srt.transl.x = currPos[0].x;
+        self->srt.transl.z = currPos[0].z;
+
+        objdata->collider.hitsPrevPos[0].x = currPos[0].x;
+        objdata->collider.hitsPrevPos[0].z = currPos[0].z;
+        objdata->collider.hitsPrevPos[0].y = self->srt.transl.y;
+    } else {
+        // Being held, don't do resolution
+        objdata->collider.hitsPrevPos[0] = self->srt.transl;
+    }
+
+    // Recalculate global position
+    transform_point_by_object(
+        self->srt.transl.x, self->srt.transl.y, self->srt.transl.z, 
+        &self->globalPosition.x, &self->globalPosition.y, &self->globalPosition.z, 
+        self->parent);
+
+    // Create AABB to test for collisions within
+    Vec3f currPos[1] = {self->globalPosition};
+    f32 radius[1] = {objdata->collider.col.unk40[0]};
+    radius[0] = sqrtf((2 * radius[0]) * radius[0]);
+
+    fit_aabb_around_cubes(&objdata->collider.aabb, currPos, objdata->collider.trackPrevPos, radius, 1);
+
+    // Do track collision detection
+    func_80053750(self, &objdata->collider.aabb, 1);
+
+    if (pickupState == PICKUP_NotHeld) {
+        // Do track collision resolution
+        Vec3f newPos[1] = {self->globalPosition};
+        objdata->collider.col.unk50[0] = -1;
+
+        func_8005509C(self, 
+            objdata->collider.trackPrevPos[0].f, newPos[0].f, 1, 
+            &objdata->collider.col, 0);
+
+        objdata->collider.trackPrevPos[0] = newPos[0];
+
+        // Note: Don't update y coord here, the pickup DLL and float logic will handle that
+        self->globalPosition.x = objdata->collider.trackPrevPos[0].x;
+        self->globalPosition.z = objdata->collider.trackPrevPos[0].z;
+    } else {
+        // Being held, don't do resolution
+        objdata->collider.trackPrevPos[0] = self->globalPosition;
+        objdata->collider.col.unk50[0] = -1;
+    }
+
+    // Recalculate local position
+    inverse_transform_point_by_object(
+        self->globalPosition.x, self->globalPosition.y, self->globalPosition.z, 
+        &self->srt.transl.x, &self->srt.transl.y, &self->srt.transl.z, 
+        self->parent);
 }
 
 static void SHbarrel_control(Object* self) {
@@ -115,23 +257,32 @@ static void SHbarrel_control(Object* self) {
 
     s32 pickupState = gDLL_54_pickup->vtbl->get_state(self->data);
     if (pickupState == PICKUP_Held) {
+        // Hacky, better to do this in the player DLL some day
         SHbarrel_drop_if_player_underwater(self);
     }
 
     pickupState = gDLL_54_pickup->vtbl->control(self);
     switch (pickupState) {
         case PICKUP_NotHeld:
-            SHbarrel_not_held_control(self, objdata);
+            SHbarrel_physics(self, objdata);
+            // Don't allow pickup while in deep water
+            self->unkAF &= ~ARROW_FLAG_8_No_Targetting;
+            if (objdata->inDeepWater) {
+                self->unkAF |= ARROW_FLAG_10_Greyed_Out;
+            } else {
+                self->unkAF &= ~ARROW_FLAG_10_Greyed_Out;
+            }
             break;
         case PICKUP_Held:
         default:
-            objdata->collider.mode = DLL27MODE_DISABLED;
+            // Reset velocity while held
+            self->velocity.x = 0.0f;
+            self->velocity.y = 0.0f;
+            self->velocity.z = 0.0f;
             break;
     }
 
-    gDLL_27->vtbl->func_1E8(self, &objdata->collider, gUpdateRateF); // update aabb
-    gDLL_27->vtbl->func_5A8(self, &objdata->collider); // do track intersect
-    gDLL_27->vtbl->func_624(self, &objdata->collider, gUpdateRateF); // collision resolution
+    SHbarrel_collision(self, objdata, pickupState);
 }
 
 static void SHbarrel_update(Object* self) {}
