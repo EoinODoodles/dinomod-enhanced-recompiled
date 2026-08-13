4c7e38b94220c4971cc0ea0e27a257218dd4343d
EoinODoodles <e.odrisceoil@gmail.com>
2026-08-13 19:28:54 +0100

    Walled City: directional objGroup handling on TriggerPlanes

--- dinomod_enhanced/src/assets.c
+++ dinomod_enhanced/src/assets.c
@@ -82,6 +82,27 @@ INCBIN(objects_vampirebat,      "inc/objects_0053_VampireBat.bin");
 #define COORDS_SETUP(coordX, coordY, coordZ) .base.x = coordX, .base.y = coordY, .base.z = coordZ
 #define TRIGGER_YAW(degrees) ((u8)((float)degrees*0x10/90.0f + 0.5f)) //Yaw for TriggerPlanes etc. (other axes use DEGREES_TO_ANGLE8)
 #define TRIGGER_SCALE(scaleFloat) ((u8)(scaleFloat*0x10 + 0.5f))
+#define GET_TRIGGER(mapID, uID) ((Trigger_Setup*)reasset_map_objects_get(mapID, reasset_base_id(uID), NULL))
+
+//A quick way to add ObjectGroup on/off commands to Trigger Objects (enter: on, exit: off)
+#define DIRECTIONAL_OBJGROUP_TRIGGER(groupID, triggerObject, cmdSlotIn, cmdSlotOut)\
+    triggerObject->commands[cmdSlotIn].condition = CMD_COND_IN | CMD_COND_RE_ENTER;\
+    triggerObject->commands[cmdSlotIn].id = TRG_CMD_ENABLE_OBJ_GROUP;\
+    triggerObject->commands[cmdSlotIn].paramCombined = groupID;\
+    \
+    triggerObject->commands[cmdSlotOut].condition = CMD_COND_OUT | CMD_COND_RE_EXIT;\
+    triggerObject->commands[cmdSlotOut].id = TRG_CMD_DISABLE_OBJ_GROUP;\
+    triggerObject->commands[cmdSlotOut].paramCombined = groupID;
+
+//A quick way to add ObjectGroup on/off commands to Trigger Objects (enter: off, exit: on)
+#define DIRECTIONAL_OBJGROUP_TRIGGER_REVERSE(groupID, triggerObject, cmdSlotIn, cmdSlotOut)\
+    triggerObject->commands[cmdSlotIn].condition = CMD_COND_IN | CMD_COND_RE_ENTER;\
+    triggerObject->commands[cmdSlotIn].id = TRG_CMD_DISABLE_OBJ_GROUP;\
+    triggerObject->commands[cmdSlotIn].paramCombined = groupID;\
+    \
+    triggerObject->commands[cmdSlotOut].condition = CMD_COND_OUT | CMD_COND_RE_EXIT;\
+    triggerObject->commands[cmdSlotOut].id = TRG_CMD_ENABLE_OBJ_GROUP;\
+    triggerObject->commands[cmdSlotOut].paramCombined = groupID;
 
 #define INCFST(fileID, filename, ext) \
     INCBIN(fst_assets_##filename##_##ext, "assets/" #filename "."#ext); \
@@ -385,6 +406,63 @@ static void walled_city_modifications(void) {
     // Delete DummyObjects in Moon/Sun temple basements (these are objects with an unmapped OBJINDEX.bin entry)
     reasset_map_objects_delete(walledCity, reasset_base_id(0x41AFC));
     reasset_map_objects_delete(walledCity, reasset_base_id(0x41B35));
+
+    // Edit objectGroup toggle TriggerPlanes to be directionally specific
+    {
+        Trigger_Setup* plane;
+
+        //Approach route
+        {
+            plane = GET_TRIGGER(walledCity, 0x40beb);
+            DIRECTIONAL_OBJGROUP_TRIGGER(5, plane, 0, 4); //90-degree bend after Queen EarthWalker's gateway
+        }
+
+        //Central temple
+        {
+            plane = GET_TRIGGER(walledCity, 0x40dad);
+            DIRECTIONAL_OBJGROUP_TRIGGER(0, plane, 2, 5); //Sun beacon temple entrance
+
+            plane = GET_TRIGGER(walledCity, 0x40dae);
+            DIRECTIONAL_OBJGROUP_TRIGGER(0, plane, 2, 5); //Sun beacon tunnel exit
+
+            plane = GET_TRIGGER(walledCity, 0x4104c);
+            DIRECTIONAL_OBJGROUP_TRIGGER(1, plane, 2, 5); //Moon beacon temple entrance
+
+            plane = GET_TRIGGER(walledCity, 0x4104f);
+            DIRECTIONAL_OBJGROUP_TRIGGER(1, plane, 2, 5); //Moon beacon tunnel exit
+
+            plane = GET_TRIGGER(walledCity, 0x41052);
+            DIRECTIONAL_OBJGROUP_TRIGGER(4, plane, 2, 3); //Boss lobby
+        }
+
+        //Sun temple route
+        {
+            plane = GET_TRIGGER(walledCity, 0x41312);
+            DIRECTIONAL_OBJGROUP_TRIGGER(2, plane, 0, 5); //Sun pushblocks approach
+            DIRECTIONAL_OBJGROUP_TRIGGER(6, plane, 6, 7); //Sun pushblocks approach
+
+            plane = GET_TRIGGER(walledCity, 0x41311);
+            DIRECTIONAL_OBJGROUP_TRIGGER(2, plane, 0, 5); //Sun temple approach
+
+            plane = GET_TRIGGER(walledCity, 0x41775);
+            DIRECTIONAL_OBJGROUP_TRIGGER_REVERSE(6, plane, 0, 1); //Sun temple entry
+            DIRECTIONAL_OBJGROUP_TRIGGER(8, plane, 6, 7); //Sun temple entry
+        }
+
+        //Moon temple route
+        {
+            plane = GET_TRIGGER(walledCity, 0x4130e);
+            DIRECTIONAL_OBJGROUP_TRIGGER(3, plane, 0, 5); //Moon pushblocks approach
+            DIRECTIONAL_OBJGROUP_TRIGGER(7, plane, 6, 7); //Moon pushblocks approach
+
+            plane = GET_TRIGGER(walledCity, 0x4130f);
+            DIRECTIONAL_OBJGROUP_TRIGGER(3, plane, 0, 6); //Moon temple approach
+
+            plane = GET_TRIGGER(walledCity, 0x41604);
+            DIRECTIONAL_OBJGROUP_TRIGGER_REVERSE(7, plane, 6, 7); //Moon temple entry
+            DIRECTIONAL_OBJGROUP_TRIGGER(9, plane, 2, 3); //Moon temple entry
+        }
+    }
 }
 
 static void shrine_fxemit_modifications(void) {
