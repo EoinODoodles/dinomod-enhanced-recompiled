7d1ff580265e90685762aa0ab05e47742d18dba1
EoinODoodles <e.odrisceoil@gmail.com>
2026-09-01 16:34:44 +0100

    WCPushBlock: decomp update

--- dinomod_enhanced/src/dlls/objects/782_WCpushblock.c
+++ dinomod_enhanced/src/dlls/objects/782_WCpushblock.c
@@ -1,37 +1,46 @@
 #include "modding.h"
 #include "recomputils.h"
 
+#include "dlls/engine/27.h"
 #include "sys/dll.h"
 #include "sys/objects.h"
 
 #include "recomp/dlls/objects/782_WCpushblock_recomp.h"
 
-typedef struct {
-/*000*/    s8 unk0[0x260 - 0];
-/*260*/    Object* unk260; //WCLevelControl
-/*264*/    f32 unk264;
-/*268*/    f32 unk268;
-/*26C*/    u32 unk26C;
-/*270*/    s16 unk270;
-/*272*/    s16 unk272;
-/*274*/    u8 unk274;
-/*275*/    u8 unk275;
-/*276*/    u8 unk276;
-/*277*/    u8 unk277;
-} WCPushBlock_Data;
-
+//TEMPORARY DEFINES
+#define WCPushBlock_obj_Setup dll_782_setup
 typedef struct {
 /*00*/ ObjSetup base;
 /*18*/ u8 unk18;
 /*19*/ s8 modelIndex;
-/*1A*/ s16 unk1A;
+/*1A*/ s16 puzzlePieceID;
 } WCPushBlock_Setup;
+//END OF TEMPORARY DEFINES
 
-//Prevents crash when the Sun Blocks loads in Walled City (originally by MusicalProgrammer)
-RECOMP_PATCH void dll_782_setup(Object* self, WCPushBlock_Setup* setup, s32 arg2) {
+typedef struct {
+/*000*/    DLL27_Data unk0;   //Unused: probably DLL27_Data
+/*260*/    Object* levelCtrl; //WCLevelControl
+/*264*/    f32 limitX;
+/*268*/    f32 limitZ;
+/*26C*/    u32 soundHandle;
+/*270*/    s16 gridX;
+/*272*/    s16 gridZ;
+/*274*/    u8 state;
+/*275*/    u8 moveDirection;
+/*276*/    u8 puzzlePieceID;  //The pushblock's identifier, used by WCLevelControl to quickly store which puzzle element is in each grid cell
+/*277*/    u8 collidedType;
+} WCPushBlock_Data;
+
+typedef enum {
+    WCPushBlock_MODELIDX_Moon,
+    WCPushBlock_MODELIDX_Sun
+} WCPushBlock_ModelIndices;
+
+//Prevents crash when the Sun Blocks load in Walled City (originally by MusicalProgrammer)
+RECOMP_PATCH void WCPushBlock_obj_Setup(Object* self, WCPushBlock_Setup* setup, s32 reset) {
     WCPushBlock_Data* objdata = self->data;
     ObjectHitInfo* hitInfo; //@recomp
-    
+
     self->opacity = 0;
     self->modelInstIdx = setup->modelIndex;
 
@@ -39,5 +48,5 @@ RECOMP_PATCH void dll_782_setup(Object* self, WCPushBlock_Setup* setup, s32 arg2
     hitInfo = self->objhitInfo;
     hitInfo->unkA0 = setup->modelIndex;
 
-    objdata->unk276 = setup->unk1A;
+    objdata->puzzlePieceID = setup->puzzlePieceID;
 }
